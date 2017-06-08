9af28c861e111b6f21e9cfc1e3acf0e79f8baee7
debbie <4debgonzalez@gmail.com>
2017-06-08 20:57:06 +0000

    pistachios calculated

--- pistachiocalc.rb
+++ pistachiocalc.rb
@@ -1,9 +1,9 @@
 require 'colorized_string'
 
 def pist_dab
-puts "how many pistachios have you eaten?"
+puts ColorizedString["how many pistachios have you eaten?"].colorize(:green)
 pistachios = gets.chomp.to_i
-puts "You should dab for #{pistachios * 180/175} minutes to burn that off. But also body positivity, so no pressure. We love you just the way you are!"
+puts ColorizedString["You should dab for #{pistachios * 180/175} minutes to burn that off. But also body positivity, so no pressure. We love you just the way you are!"].colorize(:green)
 end
 
 puts ColorizedString["Welcome to the ULTIMATE Pistachio Calculator. Please crack your first shell(number)."].colorize(:green)
@@ -23,51 +23,51 @@ def pcalc(a,op,b)
     end
     if op == "+"
         output = a + b
-        print "#{output} pistachios. "
+        print ColorizedString["#{output} pistachios. "].colorize(:green)
     elsif op == "-"
         output = a - b
-        print "#{output} pistachios. "
+        print ColorizedString["#{output} pistachios. "].colorize(:green)
     elsif op == "*"
         output = a * b
-        print "#{output} pistachios. "
+        print ColorizedString["#{output} pistachios. "].colorize(:green)
     elsif op == "/"
         output = a / b
-        print "#{output} pistachios. "
+        print ColorizedString["#{output} pistachios. "].colorize(:green)
     elsif op == "%"
         output = a % b
-        print "#{output} pistachios. "
+        print ColorizedString["#{output} pistachios. "].colorize(:green)
     elsif op == "**"
         output = a ** b
-        print "#{output} pistachios. "
+        print ColorizedString["#{output} pistachios. "].colorize(:green)
     elsif op == "root"
         output = a**(1/b)
-        print "#{output} pistachios. "
+        print ColorizedString["#{output} pistachios. "].colorize(:green)
     elsif op == "clear"
         a = 0
         b = 0
-        output = "You're out of pistachios!"
+        output = ColorizedString["You're out of pistachios!"].colorize(:green)
     elsif op == "calories"
         puts pist_dab
     else
-        puts "Wrong cracking technique(operation)"
+        puts ColorizedString["Wrong cracking technique(operation)"].colorize(:green)
     end
     if op == "clear"
         a = 0
         b = 0
-        output = "You're out of pistachios!"
+        output = ColorizedString["You're out of pistachios!"].colorize(:green)
     end
-    until op == "clear"
+    until (op == "clear") || (op == "calories")
         a = output
         puts ColorizedString["What mode of cracking(operation) would you like to use (+, -, *, /, **, %, root, clear)?"].colorize(:green)
         op = gets.chomp
             if op == "clear"
                 a = 0
                 b = 0
-                output = "You're out of pistachios!"
+                output = ColorizedString["You're out of pistachios!"].colorize(:green)
             elsif op == "calories"
             puts pist_dab
             else
-                puts "Please crack your second shell(number)."
+                puts ColorizedString["Please crack your second shell(number). If you are using a root, enter what kind (3 for cube root, 2 for square)"].colorize(:green)
                 b = gets.chomp.to_f 
                 pcalc(a,op,b)
             end
